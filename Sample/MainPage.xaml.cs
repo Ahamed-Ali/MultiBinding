998c24471ad125e283f6cc551c58c10e5f15acb0
Anandhan <anandhan.rajagopal@syncfusion.com>
2024-02-06 17:00:03 +0530

    Added sample

---
+++ Sample/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace MauiApp3
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        MyViewModel Vm;
+      
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = Vm = new MyViewModel();
+           
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            var vm = this.BindingContext as MyViewModel;
+            if (vm!= null)
+            {
+                vm.Price++;
+            }
+            
+        }
+
+        
+    }
+
+    public partial class MyViewModel : ObservableObject
+    {
+        [ObservableProperty] double _Price = 2;
+    }
+
+}
---
+++ Sample/ValueConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp3
+{
+    public class ValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is double)
+            {
+                double price = (double)values[0];
+                return price;
+            }
+
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+
+            
+        }
+
+       
+    }
+}
